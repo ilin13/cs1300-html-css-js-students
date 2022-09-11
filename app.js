c1eaed7ff54bb722220532ae939058fd2c4e3b37
Ivy <ivy_lin1@brown.edu>
2022-09-11 17:51:46 -0400

    Push

--- app.js
+++ app.js
@@ -26,14 +26,21 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
-
-  // TODO: Create a new HTML element to display your data
+  const filteredArray = fruitsArray.filter(
+    (elem) => elem.nutritions.calories > 50)
+  
+  // TODO: Create a new HTML element to display your data  
+  filteredArray.map(elem => {
+    const newItem = document.createElement("div");
+    newItem.textContent = elem.name + ', Calories: ' + elem.nutritions.calories;
 
   // TODO: Append your new element to the page
+    gallery.append(newItem)
+  });
 
 }
 
